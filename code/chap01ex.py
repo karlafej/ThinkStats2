a1168b81ef2a1d0d9599f7b5a6801a1057dbb2bc
Karla Fejfarova <karlafej@gmail.com>
2017-05-03 15:05:18 +0200

    chapter 1

--- code/chap01ex.py
+++ code/chap01ex.py
@@ -10,8 +10,39 @@ from __future__ import print_function
 import numpy as np
 import sys
 
-import nsfg
+import nsfg # if this is imported I dont need to write read functions anymore...
 import thinkstats2
+
+def ReadFemResp(dct_file='2002FemResp.dct', 
+                dat_file='2002FemResp.dat.gz',
+                nrows=None):
+    """Reads the NSFG respondent data.
+
+    dct_file: string file name
+    dat_file: string file name
+
+    returns: DataFrame
+    """
+    dct = thinkstats2.ReadStataDct(dct_file)
+    df = dct.ReadFixedWidth(dat_file, compression='gzip', nrows=nrows)
+    return df
+
+ 
+def CrossValidPregnum(resp):
+    preg = nsfg.ReadFemPreg()
+    preg_map = nsfg.MakePregMap(preg)
+
+    val = True
+
+    for index, pregnum in resp.pregnum.iteritems():
+        caseid = resp.caseid[index]
+        indices = preg_map[caseid]
+
+        if (len(indices) != pregnum):
+    	    print('Caseid: {}, Preg: {}, Resp.pregnum: {}'.format(caseid, len(indices), pregnum))
+    	    val = False
+    return val
+	
 
 
 def main(script):
@@ -19,6 +50,14 @@ def main(script):
 
     script: string script name
     """
+    resp = ReadFemResp()
+    print('Pregnancy number counts:\n')
+    print(resp.pregnum.value_counts().sort_index())
+
+    assert(len(resp) == 7643)
+    assert(resp.pregnum.value_counts()[1] == 1267)
+    assert(CrossValidPregnum(resp))
+    
     print('%s: All tests passed.' % script)
 
 
